d372d15dc8a86ab9b13e8e416b7a6fe633cab0f6
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2022-06-23 15:26:48 +0200

    ECS-194: Fix wrong check # Conflicts: #	src/main/java/org/entando/entando/plugins/jacms/aps/servlet/ProtectedResourceProvider.java

--- src/main/java/org/entando/entando/plugins/jacms/aps/servlet/ProtectedResourceProvider.java
+++ src/main/java/org/entando/entando/plugins/jacms/aps/servlet/ProtectedResourceProvider.java
@@ -23,7 +23,6 @@ import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.lang3.StringUtils;
 import org.entando.entando.aps.servlet.IProtectedResourceProvider;
 import org.entando.entando.ent.util.EntLogging.EntLogger;
 import org.entando.entando.ent.util.EntLogging.EntLogFactory;
