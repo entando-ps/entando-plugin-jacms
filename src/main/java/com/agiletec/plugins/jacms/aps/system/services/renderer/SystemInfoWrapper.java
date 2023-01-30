57d707c71841974f7e1c57c4108b54989419f1f6
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2023-01-30 11:22:12 +0100

    SMEMT-8: Added new method "resourceRootUrl" in "info" render functionality

--- src/main/java/com/agiletec/plugins/jacms/aps/system/services/renderer/SystemInfoWrapper.java
+++ src/main/java/com/agiletec/plugins/jacms/aps/system/services/renderer/SystemInfoWrapper.java
@@ -28,116 +28,120 @@ import com.agiletec.aps.system.services.page.Widget;
 import com.agiletec.aps.system.services.url.IURLManager;
 import com.agiletec.aps.system.services.url.PageURL;
 import com.agiletec.aps.util.ApsWebApplicationUtils;
+import org.entando.entando.aps.system.services.storage.IStorageManager;
 
 /**
  * @author E.Santoboni
  */
 public class SystemInfoWrapper {
 
-	private static final EntLogger _logger = EntLogFactory.getSanitizedLogger(SystemInfoWrapper.class);
-	
-	public SystemInfoWrapper(RequestContext reqCtx) {
-		this.setReqCtx(reqCtx);
-	}
-	
-    /**
-	 * Return the value of a System parameter.
-	 * @param paramName The name of parameters
-	 * @return The value to return
-	 */
-    public String getConfigParameter(String paramName) {
-		try {
-            ConfigInterface configManager = 
-					(ConfigInterface) ApsWebApplicationUtils.getBean(SystemConstants.BASE_CONFIG_MANAGER, this.getReqCtx().getRequest());
-            return configManager.getParam(paramName);
-        } catch (Throwable t) {
-        	
-        	_logger.error("Error extracting config parameter - parameter ", paramName, t);
-        	
-            //ApsSystemUtils.logThrowable(t, this, "getConfigParameter", "Error extracting config parameter - parameter " + paramName);
-			return null;
+    private static final EntLogger _logger = EntLogFactory.getSanitizedLogger(SystemInfoWrapper.class);
+
+    private RequestContext _reqCtx;
+
+    public SystemInfoWrapper(RequestContext reqCtx) {
+        this.setReqCtx(reqCtx);
+    }
+
+    public String getResourceRootUrl() {
+        try {
+            IStorageManager storageManager = (IStorageManager) ApsWebApplicationUtils.getBean(SystemConstants.STORAGE_MANAGER, this.getReqCtx().getRequest());
+            return storageManager.createFullPath("", false);
+        } catch (Exception e) {
+            _logger.error("Error extracting public resource rootUrl", e);
+            return null;
         }
     }
-	
+
+    /**
+     * Return the value of a System parameter.
+     * @param paramName The name of parameters
+     * @return The value to return
+     */
+    public String getConfigParameter(String paramName) {
+        try {
+            ConfigInterface configManager
+                    = (ConfigInterface) ApsWebApplicationUtils.getBean(SystemConstants.BASE_CONFIG_MANAGER, this.getReqCtx().getRequest());
+            return configManager.getParam(paramName);
+        } catch (Throwable t) {
+            _logger.error("Error extracting config parameter - parameter ", paramName, t);
+            return null;
+        }
+    }
+
     public IPage getCurrentPage() {
-		try {
+        try {
             IPage page = (IPage) this.getReqCtx().getExtraParam(SystemConstants.EXTRAPAR_CURRENT_PAGE);
             return page;
         } catch (Throwable t) {
-        	_logger.error("Error getting current page", t);
-            //ApsSystemUtils.logThrowable(t, this, "getCurrentPage", "Error current page");
-			return null;
+            _logger.error("Error getting current page", t);
+            return null;
         }
     }
 
     public IPage getPageWithWidget(String widgetCode) {
-    	IPage page = null;
-    	try {
+        IPage page = null;
+        try {
             IPageManager pageManager = (IPageManager) ApsWebApplicationUtils.getBean(SystemConstants.PAGE_MANAGER, this.getReqCtx().getRequest());
-    		List<IPage> pages = pageManager.getOnlineWidgetUtilizers(widgetCode);
-    		if (null != pages && !pages.isEmpty()) {
-    			page = pages.get(0);
-    		}
-    		return page;
-    	} catch (Throwable t) {
-    		_logger.error("Error getting page with widget: {}", widgetCode, t);
-    		//ApsSystemUtils.logThrowable(t, this, "getPageWithWidget", "Error getting page with widget: " + widgetCode);
-    		return null;
-    	}
+            List<IPage> pages = pageManager.getOnlineWidgetUtilizers(widgetCode);
+            if (null != pages && !pages.isEmpty()) {
+                page = pages.get(0);
+            }
+            return page;
+        } catch (Throwable t) {
+            _logger.error("Error getting page with widget: {}", widgetCode, t);
+            return null;
+        }
     }
 
     public String getPageURLWithWidget(String widgetCode) {
-    	String url = null;
-    	try {
-    		IPage page = this.getPageWithWidget(widgetCode);
-    		if (null == page) return url;
-    		IURLManager urlManager = (IURLManager) ApsWebApplicationUtils.getBean(SystemConstants.URL_MANAGER, this.getReqCtx().getRequest());
-    		PageURL pageUrl = urlManager.createURL(this.getReqCtx());
-    		pageUrl.setPage(page);
-    		url = pageUrl.getURL();
-    	} catch (Throwable t) {
-    		_logger.error("Error getting pageUrl with widget: {}", widgetCode, t);
-    		//ApsSystemUtils.logThrowable(t, this, "getPageURLWithWidget", "Error getting pageUrl with widget: " + widgetCode);
-    		return null;
-    	}
-    	return url;
+        String url = null;
+        try {
+            IPage page = this.getPageWithWidget(widgetCode);
+            if (null == page) {
+                return url;
+            }
+            IURLManager urlManager = (IURLManager) ApsWebApplicationUtils.getBean(SystemConstants.URL_MANAGER, this.getReqCtx().getRequest());
+            PageURL pageUrl = urlManager.createURL(this.getReqCtx());
+            pageUrl.setPage(page);
+            url = pageUrl.getURL();
+        } catch (Throwable t) {
+            _logger.error("Error getting pageUrl with widget: {}", widgetCode, t);
+            return null;
+        }
+        return url;
     }
-    
+
     public Lang getCurrentLang() {
-		try {
+        try {
             return (Lang) this.getReqCtx().getExtraParam(SystemConstants.EXTRAPAR_CURRENT_LANG);
         } catch (Throwable t) {
-        	_logger.error("Error getting current lang", t);
-            //ApsSystemUtils.logThrowable(t, this, "getCurrentLang", "Error current lang");
-			return null;
+            _logger.error("Error getting current lang", t);
+            return null;
         }
     }
-	
-    
-    /**
-		 * @deprecated Use {@link #getCurrentWidget()} instead
-		 */
-		public Widget getCurrentShowlet() {
-			return getCurrentWidget();
-		}
 
-		public Widget getCurrentWidget() {
-			try {
-				return (Widget) this.getReqCtx().getExtraParam(SystemConstants.EXTRAPAR_CURRENT_WIDGET);
-			} catch (Throwable t) {
-				_logger.error("Error getting current Widget", t);
-				//ApsSystemUtils.logThrowable(t, this, "getCurrentWidget", "Error current Widget");
-				return null;
-			}
-		}
-	
+    /**
+     * @deprecated Use {@link #getCurrentWidget()} instead
+     */
+    public Widget getCurrentShowlet() {
+        return getCurrentWidget();
+    }
+
+    public Widget getCurrentWidget() {
+        try {
+            return (Widget) this.getReqCtx().getExtraParam(SystemConstants.EXTRAPAR_CURRENT_WIDGET);
+        } catch (Throwable t) {
+            _logger.error("Error getting current Widget", t);
+            return null;
+        }
+    }
+
     protected RequestContext getReqCtx() {
         return _reqCtx;
     }
     private void setReqCtx(RequestContext reqCtx) {
         this._reqCtx = reqCtx;
     }
-    
-	private RequestContext _reqCtx;
-	
+
 }
